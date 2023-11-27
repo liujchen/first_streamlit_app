e7aa7b3f266c962fb840945dd4ff5d1d5d4568b9
liujchen <Jingchen.Liu@cba.com.au>
2023-11-28 00:18:05 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ except URLError as e:
   streamlit.error()
 
 
-def get_fruit_load_list:
+def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
         return my_cur.fetchall()
